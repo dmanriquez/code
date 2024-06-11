336d56bfd7ab95a8a171bb378c7f4eb4a67d0937
David Manriquez <dmanriquez@bcn.cl>
2024-06-11 14:44:58 -0400

    Se agregan comentarios en las funciones

--- Modulo-Typescript/guia-preparacion-lab2.ts
+++ Modulo-Typescript/guia-preparacion-lab2.ts
@@ -31,22 +31,28 @@ console.log(clasificaParesImpares([15, 46, 92, 78, 32, 65, 17]));
 // Función para clasificar a los puntajes
 function clasificarPuntajes(puntajes: number[]):number[] {
     let puntajesClasificados: number[] = [];
-
+    // se itera sobre el array de puntajes
     for (let i = 0; i < puntajes.length; i++) {
+        // condicion para clasificar a los puntajes
         if (puntajes[i] > 35) {
+            // si el puntaje es mayor a 35 se agrega al array
             puntajesClasificados.push(puntajes[i]);
         }
     }
+    // retorna los puntajes clasificados
     return puntajesClasificados;
 }
 
 // Función para clasificar a los participantes
 function clasificarParticipantes(nombres: string[]): string[] {
+    // se crea un array vacío para almacenar los participantes clasificados
     let participantesClasificados: string[] = [];
-
+    // se itera sobre el array de nombres
     for (let i = 0; i < nombres.length; i++) {
+        // si el puntaje es mayor o igual a 35 se agrega al array
         participantesClasificados.push(nombres[i]);
       }
+    // retorna los participantes clasificados
     return participantesClasificados;
   }
 
@@ -54,14 +60,15 @@ function clasificarParticipantes(nombres: string[]): string[] {
 function encontrarMejorJugador(nombres: string[], puntajes: number[]): string {
     let mejorPuntaje = Math.max(...puntajes);
     let mejorJugador = nombres[puntajes.indexOf(mejorPuntaje)];
-
+    // retorna el nombre del mejor jugador
     return mejorJugador;
 }
 
+// Función para encontrar al peor jugador
 function encontrarPeorJugador(nombres: string[], puntajes: number[]): string{
     let peorPuntaje = Math.min(...puntajes);
     let peorJugador = nombres[puntajes.indexOf(peorPuntaje)];
-
+    // retorna el nombre del peor jugador
     return peorJugador;
 }
 
@@ -75,11 +82,18 @@ let participantes = clasificarParticipantes(NOMBRES);
 let mejorJugador = encontrarMejorJugador(NOMBRES, PUNTAJES);
 let peorJugador = encontrarPeorJugador(NOMBRES, PUNTAJES);
 
-// Imprimir los resultados
+// Imprimir los resultados con el formato requerido
 console.log("CLASIFICADOS");
 for (let i = 0; i < participantes.length; i++) {
     console.log(`${participantes[i]} \t ${puntajes[i]} puntos`);
 }
 
+// Imprimir el mejor jugador
 console.log(`Mejor Jugador: ${mejorJugador} con ${Math.max(...puntajes)} puntos.`);
+// Imprimir el peor jugador
 console.log(`Peor Jugador: ${peorJugador} con ${Math.min(...puntajes)} puntos.`);
+
+
+
+
+// Ejercicio 3
